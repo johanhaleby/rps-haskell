5e0dd4d3549f0de683a76ccc6938a4d200cd0cd9
Johan Haleby <johan.haleby@gmail.com>
2017-12-03 13:18:35 +0100

    Added request logging

--- src/Http.hs
+++ src/Http.hs
@@ -1,19 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Http (runApp, app) where
 
-import           Data.Aeson  (Value (..), object, (.=))
-import           Network.Wai (Application)
+import           Data.Aeson                           (Value (..), object, (.=))
+import           Network.Wai                          (Application)
+import           Network.Wai.Middleware.RequestLogger
 import           Web.Scotty
 
-app' :: ScottyM ()
-app' = do
-  get "/" $ do
-    setHeader "Content-Type" "text/html"
-    file "./static/index.html"
+indexPage = do
+  setHeader "Content-Type" "text/html"
+  file "./static/index.html"
+
+routes :: ScottyM ()
+routes = do
+  get "/" indexPage
   get "/some-json" $ json $ object ["foo" .= Number 23, "bar" .= Number 42]
 
 app :: IO Application
-app = scottyApp app'
+app = scottyApp routes
 
 runApp :: IO ()
-runApp = scotty 8080 app'
+runApp =
+  scotty 8080 $ do
+    -- Apply request logging
+    middleware logStdoutDev
+    -- Apply the routes
+    routes
