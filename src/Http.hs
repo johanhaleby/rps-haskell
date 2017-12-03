9fd62815a43f4ce1b6a4dc7e0d8f607100bf2b99
Johan Haleby <johan.haleby@gmail.com>
2017-12-03 13:37:11 +0100

    Added static content serving

--- src/Http.hs
+++ src/Http.hs
@@ -4,8 +4,12 @@ module Http (runApp, app) where
 import           Data.Aeson                           (Value (..), object, (.=))
 import           Network.Wai                          (Application)
 import           Network.Wai.Middleware.RequestLogger
+import           Network.Wai.Middleware.Static
 import           Web.Scotty
 
+-- The reason why we're not using "staticPolicy" for index.html is that
+-- we don't want the user to explicitly have to enter "http://uri/index.html".
+-- See https://stackoverflow.com/questions/22662826/web-scotty-file-not-found-while-serving-static-files
 indexPage = do
   setHeader "Content-Type" "text/html"
   file "./static/index.html"
@@ -23,5 +27,7 @@ runApp =
   scotty 8080 $ do
     -- Apply request logging
     middleware logStdoutDev
+    -- Apply static content
+    middleware $ staticPolicy noDots
     -- Apply the routes
     routes
